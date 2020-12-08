af90393e340ac17d5cf2f929e50d96769aed4c73
Fernando Chavez <fjchavez01@gmail.com>
2020-12-08 12:43:26 -0800

    added knex schema and made table

--- data/migrations/20201208111300_cars_table.js
+++ data/migrations/20201208111300_cars_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table => {
+      table.increments()
+      table.text('make', 50).notNullable()
+      table.text('model', 50).notNullable()
+      table.text('vin', 25)
+      table.float('milage')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars')
 };
