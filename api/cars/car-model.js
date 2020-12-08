b8d85b6537cd9c4ebfa3dabe44614e867963ca2c
Fernando Chavez <fjchavez01@gmail.com>
2020-12-08 13:45:22 -0800

    added working endpoint to create and view// MVP DONE

--- api/cars/car-model.js
+++ api/cars/car-model.js
@@ -1,1 +1,24 @@
 const db = require('../../data/dbConfig')
+
+
+
+module.exports = {
+    get,
+    getById,
+    post
+}
+
+
+function get() {
+    return db('cars')
+}
+
+function getById(id){
+    return db('cars').where('id', id).first()
+}
+
+function post(car){
+    return db('cars')
+        .insert(car, 'id')
+        .then(([id]) => get(id))
+}
--- api/server.js
+++ api/server.js
@@ -4,8 +4,13 @@ const carRouter = require('./cars/car-router')
 
 const server = express()
 
-server.use(cors)
+server.use(cors())
 server.use(express.json())
 server.use('/api/cars', carRouter)
+
+server.get('/', (_, res) => {
+    res.json({ message: 'API ONLINE'})
+})
+
 
 module.exports = server
